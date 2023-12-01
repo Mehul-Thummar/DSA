e0e308be96e9e7143af7346ae120e10f273f8692
BBS CREDIT CO-OP. SOC. LTD <bbsofcbackup@gmail.com>
2023-12-01 15:03:32 +0530

    new add

---
+++ pre_post.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+
+    printf("A: %d A: %d", a, a++);
+    printf("\nA: %d A: %d", a, ++a);
+    printf("\nA: %d A: %d", a++, a++);
+    printf("\nA: %d A: %d", a++, ++a);
+    printf("\nA: %d A: %d", ++a, ++a);
+    printf("\nA: %d A: %d A: %d", a, a++, a++);
+    printf("\nA: %d A: %d A: %d", a, ++a, a++);
+    printf("\nA: %d A: %d A: %d", a++, ++a, a++);
+    printf("\nA: %d A: %d A: %d", a++, ++a, ++a);
+    printf("\nA: %d A: %d A: %d", ++a, ++a, ++a);
+    printf("\nA: %d A: %d A: %d", ++a, ++a, a++);
+    printf("\nA: %d A: %d A: %d", a++, ++a, a++);
+    printf("\nA: %d A: %d A: %d", ++a, a++, ++a);
+    printf("\nA: %d A: %d A: %d A: %d", a++, ++a, a++, ++a);
+
+    // two array merge
+
+    // 10 20 30 40 50 -- > 50 20 30 40 10
+
+    // 10 20 30 40 50 -- > 20 30 40 50 10
+}
