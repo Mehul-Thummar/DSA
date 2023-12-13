2b54742333db40276decb662a73e384203fe1376
BBS CREDIT CO-OP. SOC. LTD <bbsofcbackup@gmail.com>
2023-12-13 16:44:40 +0530

    new add

--- recursion_binary.c
+++ recursion_binary.c
@@ -8,89 +8,60 @@ void bubble_sort(int arr[], int n)
 
 {
     int temp, i, j;
-
     for (i = 0; i < n; i++)
     {
         for (j = i; j < n; j++)
         {
             if (arr[i] > arr[j])
             {
-                temp = arr[i];
-
-                arr[i] = arr[j];
-
-                arr[j] = temp;
+                arr[j] = arr[i] + arr[j];
+                arr[i] = arr[j] - arr[i];
+                arr[j] = arr[j] - arr[i];
             }
         }
     }
 }
 
-void binary_search(int arr[], int min, int max, int key)
-
+void binary_search(int arr[], int min, int max, int result)
 {
-
     int mid;
-
     if (min > max)
-
     {
-
-        printf("Key not found\n");
-
+        printf("Number not found");
         return;
     }
-
     mid = (min + max) / 2;
 
-    if (arr[mid] == key)
-
+    if (arr[mid] == result)
     {
-
-        printf("Key found\n");
+        printf("Number found");
     }
-
-    else if (arr[mid] > key)
-
+    else if (arr[mid] > result)
     {
-
-        binary_search(arr, min, mid - 1, key);
+        binary_search(arr, min, mid - 1, result);
     }
-
-    else if (arr[mid] < key)
-
+    else if (arr[mid] < result)
     {
-
-        binary_search(arr, mid + 1, max, key);
+        binary_search(arr, mid + 1, max, result);
     }
 }
+
 int main()
-
 {
-
-    int key, n, i;
-
+    int result, n, i;
     int arr[25];
 
-    printf("Enter n of a arr: ");
-
+    printf("Enter Number of array:");
     scanf("%d", &n);
-
-    printf("Enter elements\n");
-
     for (i = 0; i < n; i++)
-
     {
-
+        printf("Enter elements:");
         scanf("%d", &arr[i]);
     }
-
     bubble_sort(arr, n);
 
-    printf("\n");
+    printf("Enter Number to search:");
+    scanf("%d", &result);
 
-    printf("Enter key to search\n");
-
-    scanf("%d", &key);
-
-    binary_search(arr, 0, n, key);
+    binary_search(arr, 0, n, result);
 }
